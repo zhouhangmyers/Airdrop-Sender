35ac10fc36e3388368fe5e2a9a25234377f03610
zhouhangmyers <zhouhangmyers@gmail.com>
2025-05-19 01:21:21 +0800

    AirDrop Sender Webset

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  distDir: "ot",
+  distDir: "out",
   images: {
     unoptimized: true
   },
